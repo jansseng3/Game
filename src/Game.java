64cb81598e6b97834ea9f720b58ba3b8628a5345
Janssen Gallenero <jansseng3@nycstudents.net>
2025-10-07 12:35:14 -0400

    page 5

---
+++ src/Game.java
@@ -0,0 +1,21 @@
+public class Game {
+    private Level levelOne;
+    private Level levelTwo;
+    private Level levelThree;
+    private boolean bonus;
+
+    levelOne = new Level();
+    levelTwo = new Level();
+    levelThree - new Level();
+
+    private Level levelOne;
+
+    // idk if this goes in game or level -- check later
+    public boolean isBonus() {
+        return bonus;
+    }
+
+    public void makeBonus() {
+        bonus = true;
+    }
+}
---
+++ src/Level.java
@@ -0,0 +1,37 @@
+public class Level {
+    private boolean goalReached;
+    private int points;
+
+    public boolean isGoalReached() {
+        return goalReached;
+    }
+
+    public void reachGoal() {
+        goalReached = true;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int, p) {
+        points = p;
+    }
+
+    public Level getLevel(int i) {
+        if (i == 1) return levelOne;
+        if (i == 2) return levelTwo;
+        if (i == 3) return levelThree;
+        return null;
+    }
+
+    public int getScore() {
+        int points = 0;
+        if (levelOne.goalReached()) {
+            points += levelOne.getPoints();
+            if (levelTwo.goalReached()) {
+                points += levelTwo.getPoints();
+            }
+        }
+    }
+}
