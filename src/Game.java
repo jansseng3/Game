1df83e98d56ab6dc7b420f8488a59fe30990028f
Janssen Gallenero <jansseng3@nycstudents.net>
2025-10-14 12:32:10 -0400

    FINISH!!

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Game {
     private Level levelOne;
     private Level levelTwo;
@@ -7,12 +9,9 @@ public class Game {
     public Game() {
         levelOne = new Level();
         levelTwo = new Level();
-        levelThree - new Level();
+        levelThree = new Level();
     }
 
-    private Level levelOne;
-
-    // idk if this goes in game or level -- check later
     public boolean isBonus() {
         return bonus;
     }
@@ -21,4 +20,46 @@ public class Game {
         bonus = true;
     }
 
+    public Level getLevel(int i) {
+        if (i == 1) return levelOne;
+        if (i == 2) return levelTwo;
+        if (i == 3) return levelThree;
+        return null;
+    }
+
+    public int getScore() {
+        int points = 0;
+        if (levelOne.goalReached()) {
+            points += levelOne.getPoints();
+            if (levelTwo.goalReached()) {
+                points += levelTwo.getPoints();
+                if (isBonus()) {
+                    points *= 3;
+                }
+            }
+        }
+        return points;
+    }
+
+    public void play () {
+        System.out.println("Enter the number of points.");
+        Scanner s = new Scanner(System.in);
+        levelOne.setPoints(s.nextInt());
+        levelOne.reachGoal();
+
+    }
+
+
+    public int playManyTimes (int num) {
+        int max = 0;
+        while (num > 0) {
+            play();
+            int score = getScore();
+            if (score > max)
+                max = score;
+                num--;
+        }
+        return max;
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Game one = new Game();
-        one.getLevel(1).reachGoal();
-        one.getLevel(1).setPoints(200);
-        one.getLevel(2).reachGoal();
-        one.getLevel(2).reachGoal(100);
-        one.getLevel(3).reachGoal();
-        one.getLevel(3).setPoints(500);
-        one.makeBonus();
+        Game two = new Game();
+        two.getLevel(1).reachGoal();
+        two.getLevel(1).setPoints(200);
+        two.getLevel(2).reachGoal();
+        two.getLevel(2).setPoints(100);
+        two.getLevel(3).setPoints(500);
+        System.out.println(two.getScore());
+        Game five = new Game();
+        System.out.println(five.playManyTimes(4));
     }
 }
